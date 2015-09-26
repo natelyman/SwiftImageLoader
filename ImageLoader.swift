88167019c4340cd72cb9014305bfc39b0acd96da
jhon.cruz <jhon@beepquest.com>
2015-09-25 19:49:58 -0500

    Swift 2.0 compatibility

--- ImageLoader.swift
+++ ImageLoader.swift
@@ -20,36 +20,36 @@ class ImageLoader {
         return Static.instance
     }
     
-    func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
-            let data: NSData? = self.cache.objectForKey(urlString) as? NSData
-            
-            if let goodData = data {
-                let image = UIImage(data: goodData)
-                dispatch_async(dispatch_get_main_queue(), {() in
-                    completionHandler(image: image, url: urlString)
-                })
-                return
-            }
-            
-            let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData!, response: NSURLResponse!, error: NSError!) -> Void in
-                if (error != nil) {
-                    completionHandler(image: nil, url: urlString)
-                    return
-                }
-                
-                if data != nil {
-                    let image = UIImage(data: data)
-                    self.cache.setObject(data, forKey: urlString)
-                    dispatch_async(dispatch_get_main_queue(), {() in
-                        completionHandler(image: image, url: urlString)
-                    })
-                    return
-                }
-                
-            })
-            downloadTask.resume()
-        })
-        
-    }
+	func imageForUrl(urlString: String, completionHandler:(image: UIImage?, url: String) -> ()) {
+		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), {()in
+			let data: NSData? = self.cache.objectForKey(urlString) as? NSData
+			
+			if let goodData = data {
+				let image = UIImage(data: goodData)
+				dispatch_async(dispatch_get_main_queue(), {() in
+					completionHandler(image: image, url: urlString)
+				})
+				return
+			}
+			
+			let downloadTask: NSURLSessionDataTask = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: urlString)!, completionHandler: {(data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+				if (error != nil) {
+					completionHandler(image: nil, url: urlString)
+					return
+				}
+				
+				if let data = data {
+					let image = UIImage(data: data)
+					self.cache.setObject(data, forKey: urlString)
+					dispatch_async(dispatch_get_main_queue(), {() in
+						completionHandler(image: image, url: urlString)
+					})
+					return
+				}
+				
+			})
+			downloadTask.resume()
+		})
+		
+	}
 }
